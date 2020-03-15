bc8ba09701887430d9d908c11c605f56901b7210
piMateusz <pilecki.mat@gmail.com>
2020-03-15 18:22:32 +0100

    'fd'

--- statystyka.m
+++ statystyka.m
@@ -1,7 +1,11 @@
-[X,cmap] = imread('corn.tif');
-% The indexed image X is a 415-by-312 array of type uint8. The colormap cmap is a 256-by-3 matrix of type double
-% therefore there are 256 colors in the indexed image. Display the image.
-imshow(X,cmap)
+tiffs = []
+for x = 1:12
+    tiff_file = 'ISO6400-' + str(x) + '.tiff'
+    [tiffs(x),cmap] = imread(tiff_file);
+    % The indexed image tiffs is a 415-by-312 array of type uint8. The colormap cmap is a 256-by-3 matrix of type double
+    % therefore there are 256 colors in the indexed image. Display the image.
+    imshow(tiffs(x),cmap)
+end
 
 % Convert the indexed image to an RGB image. The result is a 415-by-312-by-3 array of type double.
 RGB = ind2rgb(X,cmap);
