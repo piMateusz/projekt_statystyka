37f773404eea61edb332edb2b7116a819d008bbc
piotr-pawelczak <58257448+piotr-pawelczak@users.noreply.github.com>
2020-03-16 16:42:49 +0100

    plot 3d

---
+++ plot3d.m
@@ -0,0 +1,23 @@
+%% Stworzenie wykresów 3d poszczególnych obrazków
+
+for i = 1:10
+    figure(i)
+    matrix = reshape(iso6400(i,:), [100 100]);
+    surf(matrix)
+    colormap('gray')
+    title(strcat('ISO6400-num', int2str(i)))
+    xlabel('wspolrzedna x piksela')
+    ylabel('wspolrzedna y piksela')
+    zlabel('odcien')
+end
+
+for i = 1:10
+    figure(i+10)
+    matrix = reshape(iso12800(i,:), [100 100]);
+    surf(matrix)
+    colormap('gray')
+    title(strcat('ISO12800-num', int2str(i)))
+    xlabel('wspolrzedna x piksela')
+    ylabel('wspolrzedna y piksela')
+    zlabel('odcien')
+end
