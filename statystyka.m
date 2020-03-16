ac3abc5a6eebccef06f4f7774594806c42792231
piMateusz <pilecki.mat@gmail.com>
2020-03-16 15:30:48 +0100

    zeby usunac pliki

--- statystyka.m
+++ statystyka.m
@@ -1,8 +1,1 @@
-ISO_6400 = []
-ISO_12800 = []
-for x = 1:12
-    tiff_file_6400 = 'ISO6400-' + str(x) + '.tiff'
-    ISO_6400(x) = load_image(tiff_file_6400) 
-    tiff_file_12800 = 'ISO12800-' + str(x) + '.tiff'
-    ISO_12800(x) = load_image(tiff_file_12800) 
-end
+
